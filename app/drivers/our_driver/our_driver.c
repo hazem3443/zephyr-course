28a0744d85300619eb885219d7da3a9eebc3c7c5
Hazem <hazemkhaled3443@gmail.com>
2026-05-31 04:47:11 +0300

    update our_driver to include GPIO handling preparing for l6-task1

--- app/drivers/our_driver/our_driver.c
+++ app/drivers/our_driver/our_driver.c
@@ -1,3 +1,4 @@
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
@@ -5,33 +6,62 @@
 
 LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
 
+struct our_driver_data {
+    struct gpio_dt_spec led;
+};
+
+/* sample_fetch: turn the LED ON */
+static int sample_fetch_my_impl(const struct device *dev,
+                                enum sensor_channel chan)
+{
+    struct our_driver_data *data = dev->data;
+
+    LOG_INF("sample_fetch: LED on (channel %d)", chan);
+    gpio_pin_set_dt(&data->led, 0);
+    k_msleep(1000);
+    return 0;
+}
+
+/* channel_get: turn the LED OFF */
 static int channel_get_my_impl(const struct device *dev,
                                enum sensor_channel chan,
                                struct sensor_value *val)
 {
-    LOG_INF("channel_get called for channel %d", chan);
+    struct our_driver_data *data = dev->data;
+
+    LOG_INF("channel_get: LED off (channel %d)", chan);
+    gpio_pin_set_dt(&data->led, 1);
+    k_msleep(1000);
     return 0;
 }
 
-static int sample_fetch_my_impl(const struct device *dev,
-                                enum sensor_channel chan)
-{
-    LOG_INF("sample_fetch called for channel %d", chan);
-    return 0;
-}
-
-//init function
 static int init(const struct device *dev)
 {
-    LOG_INF("our driver is initialized!");
+    struct our_driver_data *data = dev->data;
+
+    if (!gpio_is_ready_dt(&data->led)) {
+        LOG_ERR("LED GPIO not ready");
+        return -ENODEV;
+    }
+    gpio_pin_configure_dt(&data->led, GPIO_OUTPUT_INACTIVE);
+
+    LOG_INF("our driver initialized");
     return 0;
 }
 
 static DEVICE_API(sensor, api_iomico_lecture) = {
-    .channel_get  = channel_get_my_impl,
     .sample_fetch = sample_fetch_my_impl,
+    .channel_get  = channel_get_my_impl,
 };
 
-#define DEV_INST(inst) DEVICE_DT_INST_DEFINE(inst, init, NULL, NULL, NULL, POST_KERNEL, 80, &api_iomico_lecture);
+#define OUR_DRIVER_DATA(inst) \
+    static struct our_driver_data our_driver_data_##inst = { \
+        .led = GPIO_DT_SPEC_INST_GET(inst, gpios), \
+    };
+
+#define DEV_INST(inst) \
+    OUR_DRIVER_DATA(inst) \
+    DEVICE_DT_INST_DEFINE(inst, init, NULL, &our_driver_data_##inst, NULL, \
+                          POST_KERNEL, 80, &api_iomico_lecture);
 
 DT_INST_FOREACH_STATUS_OKAY(DEV_INST);
